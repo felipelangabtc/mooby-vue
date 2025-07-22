756a046e9db56e89f4fcfe0bc2e76994c7046dba
Felipe <felipelangabtc@outlook.com>
2025-07-22 20:25:13 +1000

    Ajustando layout

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -5,14 +5,14 @@
 <template>
     <form @submit.prevent="props.cadastraTarefa">
         <div class="row">
-            <div class="col">
+            <div class="col-md">
                 <input :value="props.tarefaTemp" @change="editaTarefaTemp" required
                     type="text" placeholder="Digite aqui a descrição da tarefa." class="form-control">
             </div>
             <div class="col-md-2">
                 <button type="submit" class="btn btn-primary">Cadastrar</button>
             </div>
-            <div class="col-md-">
+            <div class="col-md-2">
                 <select @change="props.trocarFiltro" class="form-control">
                     <option value="todas">Todas tarefas</option>
                     <option value="pendentes">Pendentes</option>
